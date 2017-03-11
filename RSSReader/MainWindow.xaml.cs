270862f8a2053576695c18ce6519917b0b8e34a6
Nira <Nira@DESKTOP-G1J3MPL>
2017-03-11 10:18:16 +0100

    Add project files.

---
+++ RSSReader/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using SimpleFeedReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSSReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+           // var posts = new Reader().ReadFeed(@"http://www.nytimes.com/services/xml/rss/nyt/International.xml");
+           // Console.WriteLine(posts.ToList().Count);
+          //  Console.ReadLine();
+          //  FeedBox.Text = posts.ToList().ToString();
+
+            var reader = new FeedReader();
+            var items = reader.RetrieveFeed("http://www.nytimes.com/services/xml/rss/nyt/International.xml");
+            string feeds=" " ;
+
+            foreach (var i in items)
+            {
+                
+                var x=(string.Format("{0}\t{1}",
+                        i.Date.ToString("g"),
+                        i.Title)
+                
+                );
+                feeds += x;
+            }
+            FeedBox.Text = feeds;
+        }
+    }
+}
---
+++ RSSReader/Reader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RSSReader
+{
+
+    public class Reader
+    {
+        public IEnumerable<Post> ReadFeed(string url)
+        {
+            var rssFeed = XDocument.Load(url);
+
+            var posts = from item in rssFeed.Descendants("item")
+                        select new Post
+                        {
+                            Title = item.Element("title").Value,
+                            Description = item.Element("description").Value,
+                            PublishedDate = item.Element("pubDate").Value
+                        };
+
+            return posts;
+        }
+    }
+
+    public class Post
+    {
+        public string PublishedDate;
+        public string Description;
+        public string Title;
+    }
+
+    
+}
