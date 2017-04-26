a7811396ef8c7b66ed6eb7333d8f4be75db7679c
Tomasz Mucha <TomaszMucha00@gmail.com>
2017-04-26 18:15:14 +0200

    Dodałem wywołanie funkcji xmel()

--- RSSReader/MainWindow.xaml.cs
+++ RSSReader/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace RSSReader
     {
         public MainWindow()
         {
+            //d
             RSSReader.Getter.xmel();
             InitializeComponent();
             Class.InicializeDataBase Init = new Class.InicializeDataBase();
