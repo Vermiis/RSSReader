3ea9c4a6cdb4c11bf27050e058960e8c843dd331
Tomasz Mucha <TomaszMucha00@gmail.com>
2017-05-03 14:51:04 +0200

    DataTimeConverter

---
+++ RSSReader/DataTimeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSSReader
+{
+    class DataTimeConverter
+    {
+        public static string ConvertDateTime(string DataTime)
+        {
+            DataTime = DataTime.Replace(".","-");
+            return DataTime;
+        }
+    }
+}
