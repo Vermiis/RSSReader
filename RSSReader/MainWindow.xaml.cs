4ba91e3a63deb67ffea065d673fec21235854af4
Damian <master0001@wp.pl>
2017-05-05 08:58:58 +0200

    Nowe Gui i wyświetlanie feeds

--- RSSReader/MainWindow.xaml.cs
+++ RSSReader/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity.Core.Objects;
 using System.Windows;
 
 
@@ -9,6 +10,8 @@ namespace RSSReader
     /// </summary>
     public partial class MainWindow : Window
     {
+        RSSTitle dataEntities = new RSSTitle();
+       
         public MainWindow()
         {
             InitializeComponent();
@@ -16,6 +19,26 @@ namespace RSSReader
             Class.InicializeDataBase Init = new Class.InicializeDataBase();
             Init.inicjalizeDataBase();
             DataWriter.WriteFeeds(Getter.xmel());
+            FeedRss();
+        }
+        void FeedRss()
+        {
+
+            DG_RSSTitle.ItemsSource = dataEntities.Feeds.Local;
+            foreach (var feed in dataEntities.Feeds)
+            {
+                //int a = 0;
+                //DG_RSSTitle.Items.Add = feed.Title.ToString();
+                //a++;
+            }
+            //ObjectQuery<Feed> products = dataEntities.Feeds.Local;
+
+            //var query =
+            //from feed in products
+            //where feed. == "Red"
+            //orderby product.ListPrice
+            //select new { product.Name, product.Color, CategoryName = product.ProductCategory.Name, product.ListPrice };
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
