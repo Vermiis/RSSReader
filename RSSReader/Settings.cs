63562971d82691ea8131794e50327dd199a19228
Tomasz Mucha <TomaszMucha00@gmail.com>
2017-05-12 00:12:22 +0200

    .

--- RSSReader/Settings.cs
+++ RSSReader/Settings.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,32 +43,5 @@ namespace RSSReader
             return null;
         }
 
-    }
-
-    public class TnijXML
-    {
-        public static List<string> GetLinksFromFile(string mark)
-        {
-            XmlDocument xml = new XmlDocument();
-            List<string> vals = null;
-            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "user.config");
-
-
-            xml.Load(fileName);
-            // xml.LoadXml(myXmlString);
-
-            //"/ArrayOfString" lub "value"
-
-            XmlNodeList xnList = xml.SelectNodes(mark);
-            foreach (XmlNode xn in xnList)
-            {
-                string val = xn["String"].InnerText;
-
-                vals.Add(val);
-            }
-            return vals;
-            //jak time to trzeba będzie brać item zerowy
-
-        }
     }
 }
